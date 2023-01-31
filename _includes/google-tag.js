69b6e39d23a5c0de817a0a930d0524646b76426b
Ugnivenko <110766390+Ugnivenko@users.noreply.github.com>
2023-02-01 08:32:23 +1000

    cookie banner js. pb

---
+++ _includes/google-tag.js
@@ -0,0 +1,8 @@
+
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){window.dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'GA_MEASUREMENT_ID');
+</script>
